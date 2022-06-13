d18d197befd4aac0edfedffc95c09997204ead94
Jack <jacopo.derosa00@gmail.com>
2022-06-13 11:06:33 +0100

    Fixed audio player in log.

--- Assets/Scripts/Audio Logs/AudioLogInspector.cs
+++ Assets/Scripts/Audio Logs/AudioLogInspector.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SpaceHorror.UI;
 
 
 namespace SpaceHorror.AudioLogs
 {
     public class AudioLogInspector : MonoBehaviour
     {
-       
+        [SerializeField] private AudioPlayer _audioPlayer;
+
+
+        public void ResetInspector()
+        {
+            _audioPlayer.ResetPlayer();
+        }
     }
 }
