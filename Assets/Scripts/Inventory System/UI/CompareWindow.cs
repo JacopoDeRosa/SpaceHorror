0651c52f6e01b9d6a491305f57b58f6bbb8e65fc
Jack <jacopo.derosa00@gmail.com>
2022-06-14 12:38:20 +0200

    Started inventory compare window.

---
+++ Assets/Scripts/Inventory System/UI/CompareWindow.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+
+namespace SpaceHorror.InventorySystem.UI
+{
+    public class CompareWindow : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        [SerializeField] private TMP_Text _title;
+        [SerializeField] private InventoryWindow _window;
+        [SerializeField] private Image _header;
+
+        private Vector2 _dragOffset;
+        private bool _dragging;
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if(RectTransformUtility.RectangleContainsScreenPoint(_header.rectTransform, eventData.position))
+            {
+                _dragOffset = new Vector2(transform.position.x, transform.position.y) - eventData.position;
+                _dragging = true;
+            }
+       
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (_dragging)
+            {
+                transform.position = eventData.position + _dragOffset;
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _dragging = false;
+        }
+
+        public void Close()
+        {
+
+        }
+    }
+}
