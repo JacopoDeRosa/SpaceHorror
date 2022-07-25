223fa3e2fca5f52658aaa8180a7c9d08b4f130cd
Jack <jacopo.derosa00@gmail.com>
2022-07-25 10:25:12 +0100

    Changes to draggable.

--- Assets/Scripts/FPS Controller/Interaction/Draggable.cs
+++ Assets/Scripts/FPS Controller/Interaction/Draggable.cs
@@ -7,16 +7,22 @@ using KinematicPhysics;
 
 namespace FPS.Interaction
 {
-    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Rigidbody), typeof(KinematicVelocity), typeof(CharacterController))]
     public class Draggable : MonoBehaviour
     {
         [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private CharacterController _dragController;
+        [SerializeField] private KinematicVelocity _kinematicVelocity;
 
         public Rigidbody Rigidbody { get => _rigidbody; }
+        public CharacterController DragController { get => _dragController; }
+        public KinematicVelocity KinematicVelocity { get => _kinematicVelocity; }
 
         private void OnValidate()
         {
             if (_rigidbody == null) _rigidbody = GetComponent<Rigidbody>();
+            if (_dragController == null) _dragController = GetComponent<CharacterController>();
+            if (_kinematicVelocity == null) _kinematicVelocity = GetComponent<KinematicVelocity>();
         }
 
         public void StartDrag()
